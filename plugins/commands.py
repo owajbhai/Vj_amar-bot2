c4d865a87739a64d49e9003aa71751c9f4b4d30c
owajbhai <owajshaikhrcm@gmail.com>
2025-05-03 22:51:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -92,7 +92,9 @@ async def start(client, message):
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [[
-            InlineKeyboardButton("🌜𝐉𝐨𝐢𝐧 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥🌛", url=invite_link.invite_link)
+            InlineKeyboardButton("⚡𝐉𝐨𝐢𝐧 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥⚡", url=invite_link.invite_link),
+            InlineKeyboardButton("⚡𝐉𝐨𝐢𝐧 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥⚡", url=invite_link.invite_link),
+            InlineKeyboardButton("⚡𝐉𝐨𝐢𝐧 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥⚡", url=invite_link.invite_link)
         ]]
         if message.command[1] != "subscribe":
             if REQUEST_TO_JOIN_MODE == True:
